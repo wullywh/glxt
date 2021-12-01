49d0ff64b16bfc18db993aeeeaa5996a308ead32
songjing890 <1508237452@qq.com>
2021-12-01 19:35:36 +0800

    分类参数

--- src/components/spgl/params.vue
+++ src/components/spgl/params.vue
@@ -1,28 +1,77 @@
 <template>
-    <div>
-        分类参数
+  <div class="wrap">
+    <!-- 头部警告信息 -->
+    <el-alert
+      title="注意：只允许为第三级分类设置相关参数！"
+      type="warning"
+      show-icon
+      :closable="false"
+    >
+    </el-alert>
+    <!-- 选择商品分类 -->
+    <div class="block">
+      <span class="demonstration">选择商品分类：</span>
+      <el-cascader
+        v-model="value"
+        :options="options"
+        :props="{ label: 'cat_name', value: 'cat_id' }"
+        @change="handleChange"
+      ></el-cascader>
+      <!-- 动态参数和静态属性 -->
+      <el-tabs v-model="activeName" @tab-click="handleClick" class="nav_sess">
+        <el-tab-pane label="动态参数" name="first">动态参数</el-tab-pane>
+        <el-tab-pane label="静态属性" name="second">静态属性</el-tab-pane>
+      </el-tabs>
     </div>
+  </div>
 </template>
 
 <script>
+import http from "../../components/http/index.js";
 export default {
-    props: {
-
+  props: {},
+  data() {
+    return {
+      // 参数导航选项卡
+      activeName: "first",
+      value: [],
+      //获取商品分类列表
+      options: [],
+    };
+  },
+  mounted() {
+    //获取商品分类列表
+    http({
+      url: "/categories",
+    }).then((res) => {
+      this.options = res.data;
+      console.log(res);
+    });
+  },
+  methods: {
+    // 选择商品分类
+    handleClick(tab, event) {
+      console.log(tab, event);
     },
-    data() {
-        return {
-
-        };
+    // 动态参数和静态属性
+    handleChange(value) {
+      console.log(value);
     },
-    methods: {
-
-    },
-    components: {
-
-    },
+  },
+  components: {},
 };
 </script>
 
 <style scoped lang="scss">
-
+.wrap {
+  padding: 20px;
+  box-sizing: border-box;
+  background-color: #fff;
+}
+.block {
+  margin: 20px 0;
+}
+.nav_sess {
+  margin-top: 20px;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,58 +5,69 @@ Vue.use(vueRouter)
 
 let router = new vueRouter({
     routes: [{
+        path: '/',
+        redirect: '/login'
+    },
+    {
+        path: '/login',
+        component: () => import('./../views/login.vue')
+    },
+    {
+        path: '/home',
+        component: () => import('./../views/Home.vue'),
+        children: [{
             path: '/',
-            redirect: '/login'
+            component: () => import('./../components/home/index.vue')
         },
         {
-            path: '/login',
-            component: () => import('./../views/login.vue')
+            path: 'users',
+            component: () => import('./../components/yhgl/users.vue')
         },
         {
-            path: '/home',
-            component: () => import('./../views/Home.vue'),
-            children: [{
-                    path: '/',
-                    component: () => import('./../components/home/index.vue')
+            path: 'roles',
+            component: () => import('./../components/qxgl/roles1.vue')
+        },
+        {
+            path: 'rights',
+            component: () => import('./../components/qxgl/rights.vue')
+        },
+        {
+            path: 'reports',
+            component: () => import('./../components/sjbb/reports.vue')
+        },
+        {
+            path: 'goods',
+            component: () => import('./../components/spgl/goods.vue')
+        },
+        {
+            path: 'addgoods',
+            component: () => import('./../components/spgl/addgoods.vue'),
+        },
+        {
+            path: 'categories',
+            component: () => import('./../components/spgl/categories.vue')
+        },
+        {
+            path: 'params',
+            component: () => import('./../components/spgl/params.vue'),
+            children: [
+
+                {
+                    path: 'dynamic',
+                    component: () => import('./../components/spgl/dynamic.vue'),
                 },
                 {
-                    path: 'users',
-                    component: () => import('./../components/yhgl/users.vue')
-                },
-                {
-                    path: 'roles',
-                    component:()=>import('./../components/qxgl/roles1.vue')
-                },
-                {
-                    path: 'rights',
-                    component: () => import('./../components/qxgl/rights.vue')
-                },
-                {
-                    path: 'reports',
-                    component: () => import('./../components/sjbb/reports.vue')
-                },
-                {
-                    path: 'goods',
-                    component: () => import('./../components/spgl/goods.vue')
-                },
-                {
-                    path: 'addgoods',
-                    component: () => import('./../components/spgl/addgoods.vue'),
-                },
-                {
-                    path: 'categories',
-                    component: () => import('./../components/spgl/categories.vue')
-                },
-                {
-                    path: 'params',
-                    component: () => import('./../components/spgl/params.vue')
-                },
-                {
-                    path: 'orders',
-                    component: () => import('./../components/ddgl/orders.vue')
+                    path: 'statics',
+                    component: () => import('./../components/spgl/statics.vue'),
                 },
             ]
-        }
+        },
+        {
+            path: 'orders',
+            component: () => import('./../components/ddgl/orders.vue')
+        },
+        ]
+    }
     ]
 })
 
